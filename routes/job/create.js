d831ed6ad7beab96382432c85bf2e195f4bfbf48
MartinKearney <martin-kearney@hotmail.co.uk>
2020-06-08 17:22:32 +0100

    Basic Template for all route files

---
+++ routes/job/create.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const Job = require('../../models/Job');
+
+// post
+
+module.exports = router;
---
+++ routes/job/update.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const Job = require('../../models/Job');
+
+// put
+
+module.exports = router;
