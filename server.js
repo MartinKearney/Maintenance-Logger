c199af246c8de642488700b5cc00bf8f5b621321
MartinKearney <martin-kearney@hotmail.co.uk>
2020-06-08 17:57:00 +0100

    Connected to db from server

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
+const connectDB = require('./db_config/db');
+const path = require('path');
 
 const app = express();
+
+// Connect to database
+connectDB();
 
 // The envirnoment variable will be used in production
 const PORT = process.env.PORT || 5000;
