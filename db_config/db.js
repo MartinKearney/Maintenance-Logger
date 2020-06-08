a89b65adeb56305e00ec1dcdc0372b3f65608f10
MartinKearney <martin-kearney@hotmail.co.uk>
2020-06-08 17:27:42 +0100

    Setup db config

---
+++ db_config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+// make environment variables available as key-value
+// pairs in 'process.env'.
+require('dotenv').config();
+
+// want to use env var instead
+const db = process.env.MONGO_URI;
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected...');
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
