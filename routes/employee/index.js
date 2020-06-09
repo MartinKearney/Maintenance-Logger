cd8b184b59798662d08d4cca10e3f8865a96c130
MartinKearney <martin-kearney@hotmail.co.uk>
2020-06-09 14:28:40 +0100

    Get all employees route working

--- routes/employee/index.js
+++ routes/employee/index.js
@@ -2,6 +2,19 @@ const express = require('express');
 const router = express.Router();
 const Employee = require('../../models/Employee');
 
-// get
+// @route     Get /employee/index
+// @desc      Get all employee details
+
+router.get('/', async (req, res) => {
+  console.log(req.body);
+
+  try {
+    const employees = await Employee.find();
+    res.json(employees);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -19,6 +19,16 @@ app.use('/employee/delete', require('./routes/employee/delete'));
 
 // Job endpoints
 
+// Serve static assets in production i.e. React
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+  // Set catch all route
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
+
 // The envirnoment variable will be used in production
 const PORT = process.env.PORT || 5000;
 
