4950dda0c806621a02d67395efd7a1fd6612847a
MartinKearney <martin-kearney@hotmail.co.uk>
2020-06-09 14:11:48 +0100

    Create employee route working

--- routes/employee/create.js
+++ routes/employee/create.js
@@ -2,6 +2,36 @@ const express = require('express');
 const router = express.Router();
 const Employee = require('../../models/Employee');
 
-// post
+// @route     Post /employee/create
+// @desc      Create a new employee record
+
+router.post('/', async (req, res) => {
+  console.log(req.body);
+
+  // Destrcuture request
+  const { firstName, lastName } = req.body;
+
+  try {
+    // Check if employee exists already
+    let employee = await Employee.findOne({ firstName, lastName });
+    if (employee) {
+      return res.status(400).json({ msg: 'Employee already exists' });
+    }
+
+    // create a new document 'employee' by using the model 'Employee'
+    employee = new Employee({
+      firstName,
+      lastName,
+    });
+
+    // save the document to the db
+    await employee.save();
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+
+  return res.status(200).json({ msg: 'New Employee Created!' });
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const app = express();
 // Connect to database
 connectDB();
 
+// Initialise middleware
+// allows us to accept json data into the API
+app.use(express.json({ extended: false }));
+
+// Define API endpoints
+// Employee endpoints
+app.use('/employees', require('./routes/employee/index'));
+app.use('/employee/create', require('./routes/employee/create'));
+app.use('/employee/delete', require('./routes/employee/delete'));
+
+// Job endpoints
+
 // The envirnoment variable will be used in production
 const PORT = process.env.PORT || 5000;
 
