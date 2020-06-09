6e50662c13df0b000d61f86f4826cdf012a69e6c
MartinKearney <martin-kearney@hotmail.co.uk>
2020-06-09 15:31:30 +0100

    Delete employee route working

--- routes/employee/delete.js
+++ routes/employee/delete.js
@@ -2,6 +2,27 @@ const express = require('express');
 const router = express.Router();
 const Employee = require('../../models/Employee');
 
-// delete
+// @route   DELETE employee/delete/:id
+// @desc    Delete a contact
+
+router.delete('/:empNum', async (req, res) => {
+  try {
+    const { empNum } = req.params;
+    console.log(empNum);
+    // attempt to find employee for deletion
+    let employee = await Employee.findOne({
+      employeeNumber: empNum,
+    });
+    console.log(employee);
+    if (!employee) return res.status(404).json({ msg: 'Employee not found' });
+
+    // remove employee from database
+    await Employee.deleteOne({ employeeNumber: empNum }, function (err) {});
+    res.status(200).json({ msg: 'Employee removed' });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
