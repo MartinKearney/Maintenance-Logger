ca1982165b63ae44958197670c5235850c1f9ed7
MartinKearney <martin-kearney@hotmail.co.uk>
2020-06-13 23:10:53 +0100

    Employee model revised

--- models/Employee.js
+++ models/Employee.js
@@ -4,7 +4,6 @@ const employeeSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
   employeeNumber: Number,
-  date: { type: String, default: Date.now },
 });
 
 module.exports = mongoose.model('Employee', employeeSchema);
